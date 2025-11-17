da2ec12bc860ca64d643e6fb7dd018043f9a5065
rookiecoder910 <carnageinside192@gmail.com>
2025-11-18 01:30:52 +0530

    second commit

---
+++ app/src/main/java/com/example/diapp/notes.kt
@@ -0,0 +1,4 @@
+package com.example.diapp
+
+//Hilt is a DI framework which is built on top of Dagger
+//it simplifies DI,reduces boilerplate code and integrate seamlessly with android components like activity,fragments and viewmodels
